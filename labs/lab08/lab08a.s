19ec4c83f08b253f5ef3ee9d3cceda9e258013e1
leticialopesms <l184423@dac.unicamp.br>
2023-09-23 11:49:30 -0300

    Avanços no lab8a.s

--- labs/lab08/lab08a.s
+++ labs/lab08/lab08a.s
@@ -1,8 +1,9 @@
 .text
 .globl _start
 
+
 _start:
-    jal main
+    j main
 
 
 exit:
@@ -10,59 +11,80 @@ exit:
     ecall
 
 
-# setPixel:
-#     li a0, 100          # x coordinate = 100
-#     li a1, 200          # y coordinate = 200
-#     li a2, 0xFFFFFFFF   # white pixel
-#     li a7, 2200         # syscall setPixel (2200)
-#     ecall
+open_file:
+    # a0 = address for the file path
+    li a1, 0        # a1 = flags (0: rdonly, 1: wronly, 2: rdwr)
+    li a2, 0        # a2 = mode
+    li a7, 1024     # a7 = syscall open (1024)
+    ecall
+    ret
 
 
-# open:
-#     la a0, input_file   # address for the file path
-#     li a1, 0            # flags (0: rdonly, 1: wronly, 2: rdwr)
-#     li a2, 0            # mode
-#     li a7, 1024         # syscall open
-#     ecall
+read_file:
+    # a0 = file descriptor
+    # a1 = buffer to write the data
+    # a2 = size (reads only 10 bytes)
+    li a7, 63       # syscall read (63)
+    ecall
+    ret
 
 
-LOOP_print:
-    # Inicializa o contador para y
-    li t2, 100
-    li a1, 1
-    LOOP_linha:
-        
-        li a7, 2200         # syscall setPixel (2200)
-        ecall
+write:
+    # a1 = buffer
+    # a2 = size
+    li a0, 1        # a0 = file descriptor = 1 (stdout)
+    li a7, 64       # a7 = syscall write (64)
+    ecall
+    ret
 
-        addi a1, a1, 1
-        # Atualiza t2
-        addi t2, t2, -1
-        bne t2, zero, LOOP_linha
 
-    addi a0, a0, 1
-    # Atualiza t1
-    addi t1, t1, -1
-    bne t1, zero, LOOP_print
+close_file:
+    # a0 = address for the file path
+    li a7, 57       # syscall close (57)
+    ecall
     ret
 
 
 main:
-    # jal open
-    # mv t0, a0   # Armazena o valor de "input_file" em t0
+    # --- Abrindo o arquivo input_file ("feep.pgm") --- #
+    # Parâmetro de "open_file":
+    la a0, input_file   # a0 = address for the file path
+    jal open_file
 
-    # Inicializa o contador para x
-    li t1, 100
-    # Inicializa parâmetros de LOOP_print
-    li a0, 1
-    li a1, 1
-    li a2, 0x000000FF
-    jal LOOP_print
-    jal exit
+    mv s0, a0       # Armazena o descritor de arquivo de "file_input" em s0
+    
+    # --- Lendo a string "header" --- #
+    # Parâmetros de "read_file":
+                    # a0 = file descriptor
+    la a1, header   # a1 = buffer to write the data
+    li a2, 15       # a2 = size (reads only a2 bytes)
+    jal read_file
 
-# .data
-# input_file: .string "bird.pgm"
-# input_file: .string "feep.pgm"
+    mv s1, a0       # Armazena o endereço de "header" em s0
 
-# OBS.: Maxval é no máximo 255
-        # Logo, cada valor de "tom" armazena 1 byte
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# PAREI AQUI
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    # --- Escrevendo no terminal do simulador --- #
+    # Parâmetros de "write":
+    la a1, header   # a1 = buffer
+    li a2, 16       # a2 = size
+    jal write
+
+    # --- Fechando o arquivo input_file ("feep.pgm") --- #
+    # Parâmetros de "close_file":    
+    mv a0, s0       # a0 = address for the file path
+    jal close_file
+    
+    j exit
+
+
+.data
+input_file: .string "feep.pgm"
+
+header: .string "P2*www*hhh*mmm*\n"
+
+# width: .string "000\n"  # max width = 512
+# height: .string "000\n" # max height = 512
+# maxval: .string "000\n" # max maxval = 255
---
+++ labs/lab08/lab08a_teste1.s
@@ -0,0 +1,73 @@
+.text
+.globl _start
+
+_start:
+    jal main
+
+
+exit:
+    li a7, 93               # syscall exit (93)
+    ecall
+
+
+# setPixel:
+#     li a0, 100          # x coordinate = 100
+#     li a1, 200          # y coordinate = 200
+#     li a2, 0xFFFFFFFF   # white pixel
+#     li a7, 2200         # syscall setPixel (2200)
+#     ecall
+#     ret
+
+
+# open:
+    # la a0, input_file   # address for the file path
+    # li a1, 0            # flags (0: rdonly, 1: wronly, 2: rdwr)
+    # li a2, 0            # mode
+    # li a7, 1024         # syscall open
+    # ecall
+    # ret
+
+
+LOOP_print:
+    # Inicializa o contador para y
+    li t1, 10
+    LOOP_linha:
+        li a7, 2200         # syscall setPixel (2200)
+        ecall
+
+        # Atualiza y
+        addi a1, a1, 1
+        # Atualiza t2
+        addi t1, t1, -1
+        bne t1, zero, LOOP_linha
+
+    # Atualiza x
+    addi a0, a0, 1
+    # Atualiza y
+    li a1, 0
+    # Atualiza t1
+    addi t0, t0, -1
+    bne t0, zero, LOOP_print
+    ret
+
+
+main:
+    # jal open
+    # mv s0, a0   # Armazena o valor de "input_file" em s0
+
+    # Inicializa o contador para x
+    li t0, 10
+    # Inicializa parâmetros de LOOP_print
+    li a0, 0    # x
+    li a1, 0    # y
+    li a2, 0x000000FF
+    jal LOOP_print
+    jal exit
+
+
+.data
+# input_file: .string "bird.pgm"
+# input_file: .string "feep.pgm"
+
+# OBS.: Maxval é no máximo 255
+        # Logo, cada valor de "tom" armazena 1 byte
